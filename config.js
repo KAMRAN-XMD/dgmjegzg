1f5d028a197c959423f5ef3713f75ea2ef94a9e1
e51908779-blip <e51908779@gmail.com>
2025-09-28 08:26:53 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "IK~6opVDTwR#GSZgFuMCOi9OYfC1aykZZJBQsJXRhoYFv7rD8JTjYVg",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
